548d0be3df044b6b79c6e4a583bcd580359b11ba
FV4005 <429664568@qq.com>
2019-07-11 14:13:02 +0800

    lodash

--- miao/lodash/fv4005-lodash.js
+++ miao/lodash/fv4005-lodash.js
@@ -2,4 +2,32 @@ var fv4005 = {
   compact: function(ary) {
     return ary.filter(it => it)
   },
+  filter: function(ary,test){
+  //数组过滤
+    if(ary === undefined){
+      ary = []
+    }
+    var passed = []
+    for(var i = 0;i < ary.length;i++){
+      if(test(ary[i])){
+        passed.push(ary[i])
+      }
+    }
+    return passed
+  },
+  concat: function(ary1,ary2) {
+//连接两个或多个数组，并将新的数组返回
+    if(ary1 === undefined){
+      ary1 = []
+    }
+    if(ary2 === undefined){
+      ary2 = []
+    }
+    var newary = []
+    for(var i = 0; i < ary2.length;i++){
+      ary1.push(ary2[i])
+    }
+    newary = newary.push(ary1)
+    return newary
+  },
 }
