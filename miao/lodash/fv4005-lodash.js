0239fc1fede9e57c0139081aee4b53ec095f93dd
FV4005 <429664568@qq.com>
2019-07-15 18:14:05 +0800

    lodash

--- miao/lodash/fv4005-lodash.js
+++ miao/lodash/fv4005-lodash.js
@@ -30,4 +30,42 @@ var fv4005 = {
     newary = newary.push(ary1)
     return newary
   },
+//深度递归遍历数组，并将所有元素与遍历到的子数组中的元素合并为一个新数组返回
+  flat:function(ary) {
+    var result = []
+    for(var i = 0; i < ary.length; i++){
+      if(Array.isArray(ary[i])){
+        for(var j = 0; j < ary[i].length; j++){
+          result.push(ary[i][j])
+        }
+      }else{
+        result.push(ary[i])
+      }
+    }
+    return result
+  },
+//判断一个数组是否包含一个指定的值
+  includes:function(ary,value){
+    for(var i = 0; i < ary.length;i++){
+      if(ary[i] === value){
+        return true
+      }else{
+        return false
+      }
+    }
+  },
+  slice:function(ary,start,end){
+    var result = []
+    if(start === undefined){
+      start == 0
+    }
+    if(end == undefined){
+      end == 0
+    }
+    for(var i = start; i < end; i++){
+      result.push(ary[i])
+    }
+    return result
+  },
+
 }
