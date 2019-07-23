7e87c95f51d343951f9ee5b0c8e8b8af16e4e3e4
FV4005 <429664568@qq.com>
2019-07-23 22:20:24 +0800

    a

--- miao/lodash/fv4005-lodash.js
+++ miao/lodash/fv4005-lodash.js
@@ -181,7 +181,7 @@ drop:function (array,n = 1){
   },
 
 dropRight: function (array,n = 1){
-    if(n === 0) return ary.slice()
+    if(n == 0) return ary
     return array.slice(0, n * - 1)
 
   },
