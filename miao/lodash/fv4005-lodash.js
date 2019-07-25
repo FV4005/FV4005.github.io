5ab9fe2e8395371f29f89d69ba2ad1278acba2f1
FV4005 <429664568@qq.com>
2019-07-25 22:29:52 +0800

    a

--- miao/lodash/fv4005-lodash.js
+++ miao/lodash/fv4005-lodash.js
@@ -257,13 +257,12 @@ indexOf:function (array, value, fromIndex=0) {
     return -1
 },
 lastIndexOf:function (array,value,fromIndex = 0){
-	for (var i = array.length; i > fromIndex; i--) {
-    if (array[i] == value) {
+	for (let i = fromIndex; i >= 0; i--) {
+    if (array[i] === value) {
         return i
     }
-
-  }
-  return -1
+}
+return -1
 },
 nth:function (array,n = 0){
   if(n >= 0){
